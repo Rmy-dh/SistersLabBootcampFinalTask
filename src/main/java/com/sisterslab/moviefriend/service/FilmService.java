f8bed0796612e3475f74a245c6af324a191d2e02
Rümeysa Küçüktaşdemir <rumeysa37373@gmail.com>
2024-03-12 00:37:33 +0300

    Deleted service interface

--- src/main/java/com/sisterslab/moviefriend/service/FilmService.java
+++ src/main/java/com/sisterslab/moviefriend/service/FilmService.java
@@ -1,19 +1,125 @@
 package com.sisterslab.moviefriend.service;
 
+import com.sisterslab.moviefriend.converter.FilmConverter;
 import com.sisterslab.moviefriend.dto.request.FilmRequest;
 import com.sisterslab.moviefriend.dto.response.FilmResponse;
-
+import com.sisterslab.moviefriend.filmMovieConstants.MovieFriendConstant;
+import com.sisterslab.moviefriend.model.Film;
+import com.sisterslab.moviefriend.model.User;
+import com.sisterslab.moviefriend.repository.FilmRepository;
+import com.sisterslab.moviefriend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface FilmService {
-     FilmResponse saveFilm(FilmRequest filmRequest);
-     FilmResponse getFilmByName(String filmName);
-     List<FilmResponse> getAllFilms();
-     String updateFilmName(String name,FilmRequest filmRequest);
-     String updateFilmExplanation(String name, FilmRequest filmRequest);
-     String updateFilmScoreByName(String name, FilmRequest filmRequest);
-     void deleteFilmByName(String name);
-     FilmResponse addUseToFilmList(Long filmId, Long userId);
-     List<FilmResponse> getFilmIfScoreGreaterThan(int score);
-     List<FilmResponse> getFilmByCategory(String category);
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public  class FilmService {
+    private static final Logger LOGGER= LogManager.getLogger(FilmService.class);
+    private final FilmRepository filmRepository;
+    private final UserRepository userRepository;
+    private final FilmUserService filmUserService;
+    public FilmResponse saveFilm(FilmRequest filmRequest) {
+        if(Objects.isNull(filmRepository.findByName(filmRequest.getName()))){
+            return FilmConverter.convertToFilmResponse(filmRepository.save(FilmConverter.convertToFilm(filmRequest)));
+        }
+        LOGGER.info(filmRequest.getName()+MovieFriendConstant.ALREADYEXIST.getName());
+        return null;
+    }
+    public FilmResponse getFilmByName(String filmName) {
+        Film film= filmRepository.findByName(filmName);
+        if(Objects.nonNull(film)){
+            return FilmConverter.convertToFilmResponse(film);
+        }else{
+            LOGGER.info(filmName+MovieFriendConstant.NOTFOUND.getName());
+            return null;
+        }
+    }
+    public List<FilmResponse> getAllFilms() {
+        return filmRepository.findAll()
+                .stream()
+                .map(FilmConverter::convertToFilmResponse)
+                .collect(Collectors.toList());
+    }
+    public String updateFilmName(String name,FilmRequest filmRequest) {
+        return setValue(name,filmRequest) ?MovieFriendConstant.NAMEUPDATED.getName()
+                : MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName();
+    }
+    public String updateFilmExplanation(String name, FilmRequest filmRequest) {
+        return setValue(name,filmRequest) ? MovieFriendConstant.EXPLANATIONUPDATED.getName()
+                :MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName();
+    }
+    public String updateFilmScoreByName(String name, FilmRequest filmRequest) {
+        return setValue(name,filmRequest)?  MovieFriendConstant.SCOREUPDATED.getName()
+                : MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName();
+    }
+    public void deleteFilmByName(String name) {
+        if(Objects.nonNull(filmRepository.findByName(name)) ){
+            filmRepository.deleteById(filmRepository.findByName(name).getId());
+            LOGGER.info(name+MovieFriendConstant.DELETED.getName());
+        }
+        LOGGER.info(name+MovieFriendConstant.NOTFOUND.getName());
+    }
+    public FilmResponse addUseToFilmList(Long filmId, Long userId) {
+        Optional<Film> film=filmRepository.findById(filmId);
+        Optional<User> user=userRepository.findById(userId);
+        if((film.isPresent() && user.isPresent()) && ! filmUserService.isUserExistInUserList(film.get(),user.get())){
+            film.get().addUserToUserList(user.get());
+            filmRepository.save(film.get());
+            filmUserService.updateFilmId(filmId,userId);
+            return FilmConverter.convertToFilmResponse(filmRepository.save(film.get()));
+        }
+        LOGGER.info(filmId + MovieFriendConstant.OR.getName()+userId+MovieFriendConstant.NOTFOUND.getName());
+        return  null;
+    }
+    public List<FilmResponse> getFilmIfScoreGreaterThan(int score) {
+
+        return filmRepository.findAll()
+                .stream()
+                .filter(i->i.getScore()>score)
+                .map(FilmConverter::convertToFilmResponse)
+                .collect(Collectors.toList());
+    }
+    public List<FilmResponse> getFilmByCategory(String category) {
+        return filmRepository.findAll()
+                .stream()
+                .filter(i->i.getCategory().equals(category))
+                .map(FilmConverter::convertToFilmResponse)
+                .collect(Collectors.toList());
+    }
+    private boolean setValue(String name,FilmRequest filmRequest){
+        Film film=filmRepository.findByName(name);
+        if(Objects.nonNull(film) && Objects.nonNull(filmRequest)){
+            if(filmRequest.getScore() !=0){
+                LOGGER.info(film.getScore()+MovieFriendConstant.ARROW.getName()
+                        +filmRequest.getScore()+MovieFriendConstant.UPDATED.getName());
+                film.setScore(filmRequest.getScore());
+                filmRepository.save(film);
+                return  true;
+            }
+            if(Objects.nonNull(filmRequest.getExplanation())){
+                LOGGER.info(film.getExplanation()+MovieFriendConstant.ARROW.getName()
+                        +filmRequest.getExplanation()+MovieFriendConstant.UPDATED.getName());
+                film.setExplanation(filmRequest.getExplanation());
+                filmRepository.save(film);
+                return true;
+            }
+            if(Objects.nonNull(filmRequest.getName())){
+                LOGGER.info(film.getName()+MovieFriendConstant.ARROW.getName()
+                        +filmRequest.getName()+MovieFriendConstant.UPDATED.getName());
+                film.setName(filmRequest.getName());
+                filmRepository.save(film);
+                return true;
+            }
+        }
+        LOGGER.error(MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName());
+        return false;
+    }
 }
--- src/main/java/com/sisterslab/moviefriend/service/UserService.java
+++ src/main/java/com/sisterslab/moviefriend/service/UserService.java
@@ -1,18 +1,108 @@
 package com.sisterslab.moviefriend.service;
 
+import com.sisterslab.moviefriend.converter.UserConverter;
 import com.sisterslab.moviefriend.dto.request.UserRequest;
 import com.sisterslab.moviefriend.dto.response.UserResponse;
-
+import com.sisterslab.moviefriend.filmMovieConstants.MovieFriendConstant;
+import com.sisterslab.moviefriend.model.Film;
+import com.sisterslab.moviefriend.model.User;
+import com.sisterslab.moviefriend.repository.FilmRepository;
+import com.sisterslab.moviefriend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface UserService {
-     UserResponse saveUser(UserRequest userRequest);
-     List<UserResponse> getAllUsers();
-     UserResponse changeUserPassword(String name, String surName,String password);
-     void deleteUserByNameAndSurname(String name, String surName);
-     UserResponse getUserByName(String name);
-     UserResponse addFilmToFilmList(Long userId, Long filmId);
-     void deleteFilmFromFilmList(Long userId, Long filmId);
 
-    void markTheFilm(Long userId, Long filmId);
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private static final Logger LOGGER = LogManager.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+    private final FilmRepository filmRepository;
+    private final FilmUserService filmUserService;
+    public UserResponse saveUser(UserRequest userRequest) {
+        try{
+            if (Objects.isNull(userRepository.findByNameAndSurName(userRequest.getName(), userRequest.getSurName()))) {
+                return UserConverter.convertToUserResponse(userRepository.save(UserConverter.convertToUser(userRequest)));
+            }
+        }catch(Exception e){
+            LOGGER.error(MovieFriendConstant.USERNAMENOTCORRECT.getName()+MovieFriendConstant.ARROW.getName()+ userRequest.getUserName());
+        }
+        LOGGER.info(userRequest.getName()+MovieFriendConstant.QUOTES.getName()+userRequest.getSurName()
+                +MovieFriendConstant.ALREADYEXIST.getName());
+        return null;
+    }
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserConverter::convertToUserResponse)
+                .collect(Collectors.toList());
+    }
+    public UserResponse changeUserPassword(String name, String surName, String password) {
+        User user = userRepository.findByNameAndSurName(name, surName);
+        if (Objects.nonNull(user) && !password.isEmpty()) {
+            user.setPassword(password);
+            userRepository.save(user);
+            return UserConverter.convertToUserResponse(user);
+        }
+        LOGGER.info(name + MovieFriendConstant.QUOTES.getName()+ surName+ MovieFriendConstant.NOTFOUND.getName());
+        return null;
+    }
+    public void deleteUserByNameAndSurname(String name, String surName) {
+        User user = userRepository.findByNameAndSurName(name, surName);
+        if (Objects.nonNull(user)) {
+            userRepository.deleteById(user.getId());
+            LOGGER.info(name + MovieFriendConstant.QUOTES.getName() + surName+MovieFriendConstant.DELETED.getName());
+        }
+        LOGGER.info(name+MovieFriendConstant.QUOTES.getName()+surName+MovieFriendConstant.NOTFOUND.getName());
+    }
+    public UserResponse getUserByName(String name) {
+        User user = userRepository.findByName(name);
+        return Objects.nonNull(user) ? UserConverter.convertToUserResponse(user) : null;
+    }
+    public UserResponse addFilmToFilmList(Long userId, Long filmId) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Film> film = filmRepository.findById(filmId);
+        if ((user.isPresent() && film.isPresent()) && ! filmUserService.isFilmExistInFilmList(film.get(),user.get())) {
+            user.get().addFilmToFilmList(film.get());
+            filmUserService.updateUserId(userId, filmId);
+            LOGGER.info(film.get().getName() +MovieFriendConstant.ADDED.getName());
+            return UserConverter.convertToUserResponse(userRepository.save(user.get()));
+        }
+        LOGGER.info(MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName());
+        return null;
+    }
+    public void deleteFilmFromFilmList(Long userId, Long filmId) {
+        Optional<Film> film=filmRepository.findById(filmId);
+        Optional<User> user=userRepository.findById(userId);
+        if(film.isPresent() && user.isPresent()){
+            if(filmUserService.isFilmExistInFilmList(film.get(),user.get())){
+                user.get().deleteFilmToFilmList(film.get());
+                filmUserService.updateFilmIdForDeleteProcess(filmId,userId);
+                userRepository.save(user.get());
+                LOGGER.info(film.get().getName()+MovieFriendConstant.DELETED.getName());
+            }
+            LOGGER.info(film.get().getName()+MovieFriendConstant.NOTFOUND.getName());
+        }
+        LOGGER.error(MovieFriendConstant.GIVENINFORMATIONNOTMATCHED.getName());
+    }
+    public void markTheFilm(Long userId, Long filmId) {
+        Optional<Film> film=filmRepository.findById(filmId);
+        Optional<User> user=userRepository.findById(userId);
+        if (( film.isPresent() && user.isPresent()) && filmUserService.isFilmExistInFilmList(film.get(),user.get())){
+            film.get().setMark(MovieFriendConstant.C.getName());
+            filmRepository.save(film.get());
+            user.get().markFilm(film.get());
+            userRepository.save(user.get());
+            LOGGER.info(film.get().getName()+MovieFriendConstant.UPDATED.getName());
+        }
+    }
 }
