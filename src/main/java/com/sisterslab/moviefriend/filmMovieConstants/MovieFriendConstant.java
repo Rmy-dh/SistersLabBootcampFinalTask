d14f469f477bf50ae952a82049e34d33079017c5
Rümeysa Küçüktaşdemir <rumeysa37373@gmail.com>
2024-03-23 23:15:17 +0300

    Fixed MovieFriendConstant

--- src/main/java/com/sisterslab/moviefriend/filmMovieConstants/MovieFriendConstant.java
+++ src/main/java/com/sisterslab/moviefriend/filmMovieConstants/MovieFriendConstant.java
@@ -2,21 +2,21 @@ package com.sisterslab.moviefriend.filmMovieConstants;
 
 public enum MovieFriendConstant {
     NAMEUPDATED("Name Updated!"),
+    UPDATED(" -> UPDATED"),
     EXPLANATIONUPDATED("Explanation Updated! "),
     SCOREUPDATED( "Score Updated! "),
     DELETED(" -> DELETED"),
-    QUOTES(" "),
+    NOTDELETED("Not Deleted !"),
     ALREADYEXIST(" -> ALREADY EXIST "),
-    NOTFOUND(" -> NOT FOUND "),
     ADDED(" -> ADDED "),
-    UPDATED(" -> UPDATED"),
     C("c"),
+    O("o"),
+    QUOTES(" "),
     OR(" OR "),
     ARROW(" -> "),
     YYYYMMDD("yyyy-MM-dd"),
-    O("o"),
-    NOTUPDATED("Not Updated!"),
-    NOTDELETED("Not Deleted !"),
+    NOTUPDATED("Not Updated !"),
+    NOTFOUND(" -> NOT FOUND "),
     USERNAMENOTCORRECT("User Name not correct"),
     GIVENINFORMATIONNOTMATCHED("Given information not matched");
 
